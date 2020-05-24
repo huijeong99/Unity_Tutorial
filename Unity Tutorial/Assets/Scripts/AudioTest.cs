cbee46104c5e0b8f37ede48856bb06f082bf5fc7
hwayuya <hwayuya@gmail.com>
2020-05-24 13:04:25 +0900

    유니티 듀토리얼 16. Audio
    -유니티버전: Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/AudioTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio =GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void PlaySE()
+    {
+        int _temp=Random.Range(0, 4);
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
+    }
+}
