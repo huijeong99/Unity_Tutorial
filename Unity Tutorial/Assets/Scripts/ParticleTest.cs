6adc75aa639e878027dd4c78e1790cb744f5b81a
hwayuya <hwayuya@gmail.com>
2020-05-24 09:05:04 +0900

    유니티 듀토리얼 10. 파티클
    -유니티버전: Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/ParticleTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTest : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ps.Play();//재생
+        //ps.Stop();//멈추기
+        //ps.Pause();//정지
+        ps.Emit(100);//버스트처럼 뿜어져 나오게
+    }
+}
