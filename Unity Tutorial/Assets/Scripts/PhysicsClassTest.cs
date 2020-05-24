d0135a1dd892da045a9ac627f8216ea1f0b78bff
hwayuya <hwayuya@gmail.com>
2020-05-24 14:10:41 +0900

    유니티 듀토리얼 17. Physics  클래스
    -유니티버전: Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/PhysicsClassTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsClassTest : MonoBehaviour
+{
+
+    //[SerializeField] private LayerMask layerMask;
+    [SerializeField] private GameObject go_BulletPrefab;
+    //
+    //private float createTime = 1f;
+    //private float currentCreateTime = 0;
+
+    private float createTime = 1f;
+    private float currentCreateTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //currentCreateTime += Time.deltaTime;
+        //
+        //if (currentCreateTime >= createTime)
+        //{
+        //
+        //    currentCreateTime = 0f;
+        //    RaycastHit hitInfo;
+        //
+        //    if (Physics.Raycast(this.transform.position, this.transform.forward, out hitInfo, 10f,layerMask))
+        //    {
+        //        if (hitInfo.transform.tag == "Player")
+        //        {
+        //            Instantiate(go_BulletPrefab, transform.position, Quaternion.LookRotation(hitInfo.transform.position - transform.position));
+        //        }
+        //        Debug.Log(hitInfo.transform.name);
+        //    }
+        //}
+
+
+       
+
+        Collider[] col = Physics.OverlapSphere(transform.position, 5f);
+
+        if (col.Length > 0)
+        {
+            for (int i = 0; i < col.Length; i++)
+            {
+                Transform tf_Target = col[i].transform;
+
+                if (tf_Target.tag == "Player")
+                {
+                    Quaternion rotation = Quaternion.LookRotation(tf_Target.position - this.transform.position);
+
+                    transform.rotation = rotation;
+                    currentCreateTime += Time.deltaTime;
+
+                    if (currentCreateTime >= createTime)
+                    {
+                        // Instantiate(go_BulletPrefab, transform.position, rotation);
+                        GameObject _temp= Instantiate(go_BulletPrefab, transform.position, rotation);
+                        Physics.IgnoreCollision(_temp.GetComponent<Collider>(),tf_Target.GetComponent<Collider>()); //충돌무시
+                        currentCreateTime = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Unity Tutorial/Assets/Scripts/playerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private Transform tf_Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(tf_Target.position, Vector3.up, speed * Time.deltaTime);    
+    }
+}
