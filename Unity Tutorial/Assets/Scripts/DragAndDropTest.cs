f7a1e033bd63ed2e79c3472c2dd47156d8611dd9
hwayuya <hwayuya@gmail.com>
2020-05-24 21:28:44 +0900

    유니티 듀토리얼 23. 드래그&드롭
    -유니티버전: Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/DragAndDropTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropTest : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler,IDropHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        //Debug.Log("Drag");
+        transform.position = eventData.position;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("End Drag");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
